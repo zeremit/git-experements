9c5eac8d1bc556b03af0803f00b512a33f5c6262
dmitry.khorevich <dmitry.khorevich@altoros.com>
2014-04-17 10:26:31 +0300

    sysout21

--- tutorial/src/main/java/com/khorevich/App.java
+++ tutorial/src/main/java/com/khorevich/App.java
@@ -9,9 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-
-        System.out.println();
         
         System.out.println( "Hello" );
+
+        System.out.println();
     }
 }
